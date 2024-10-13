7c1bf3b1a43f6ad149960d7c74bf6019cd6ece82
NguyenHoangVu-2004 <vunhph45875@fpt.edu.vn>
2024-10-13 07:27:01 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  </QueryClientProvider>
 );
